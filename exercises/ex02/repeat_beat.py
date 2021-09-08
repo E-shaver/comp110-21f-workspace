023933ee932297aa10031375c9e583a109917496
Erin <eshaver@email.unc.edu>
2021-09-08 19:14:58 -0400

    Classwork and Homework progress

--- exercises/ex02/repeat_beat.py
+++ exercises/ex02/repeat_beat.py
@@ -1,6 +1,11 @@
 """Repeating a beat in a loop."""
 
-__author__ = "ENTER YOUR 9-DIGIT PID HERE"
+__author__ = "730396741"
 
 
 # Begin your solution here...
+beat: str = input("What beat do you want repeated? ")
+number: int = int(input("How many times would you like for this beat to be repeated? "))
+maximum: 
+
+while number 
